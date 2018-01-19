91e8f69e89257fbe28c14df4f9f090e4b270cceb
Kupaw Hoshidori <silver_wind@live.jp>
2018-01-18 21:01:39 -0300

    Agregar archivos de proyecto.

---
+++ Kuph/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using System.Collections.Generic;
+using Android.Widget;
+using Android.OS;
+
+namespace Kuph
+{
+    [Activity(Label = "Kuph", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private List<string> roles;
+        private ListView rolesview;
+            protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            SetContentView(Resource.Layout.Main);
+            base.OnCreate(savedInstanceState);
+
+            rolesview = FindViewById<ListView>(Resource.Id.listView1);
+
+            roles = new List<string>();
+            roles.Add("Politician");
+            roles.Add("Mafia Boss");
+            roles.Add("Reporter");
+            roles.Add("Missionary");
+            roles.Add("Customs Office");
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, roles);
+
+            rolesview.Adapter = adapter;
+
+        }
+    }
+}
+
